566546cce2a8bb897a167e1115c9978d19afcba0
dista <btx342@gmail.com>
2019-02-20 01:08:41 +0800

    format code

--- ngx_rtmp_http_live_module.c
+++ ngx_rtmp_http_live_module.c
@@ -132,14 +132,14 @@ ngx_module_t ngx_http_rtmp_live_module = {
     NGX_MODULE_V1,
     &ngx_http_rtmp_live_module_ctx,          /* module context */
     ngx_http_rtmp_live_commands,             /* module directives */
-    NGX_HTTP_MODULE,                    /* module type */
-    NULL,                               /* init master */
-    NULL,                               /* init module */
-    NULL,                               /* init process */
-    NULL,                               /* init thread */
-    NULL,                               /* exit thread */
-    NULL,                               /* exit process */
-    NULL,                               /* exit master */
+    NGX_HTTP_MODULE,                         /* module type */
+    NULL,                                    /* init master */
+    NULL,                                    /* init module */
+    NULL,                                    /* init process */
+    NULL,                                    /* init thread */
+    NULL,                                    /* exit thread */
+    NULL,                                    /* exit process */
+    NULL,                                    /* exit master */
     NGX_MODULE_V1_PADDING
 };
 
